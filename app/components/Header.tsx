e984ff6261136f95ab369153b9577e016a627627
pawelek <k.pawelek@vp.pl>
2025-01-17 21:31:51 +0100

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,18 @@
 import { NavLink, navLinks } from "../constants/arrays/navLinks";
 import { LinkComponent as Link } from "./Link";
+import { Logo } from "./Logo";
 
 export const Header = () => {
   return (
     <header className="p-4 w-full flex justify-between">
-      {navLinks.map(({ to, label }: NavLink) => (
-        <Link key={label} to={to}>
-          {label}
-        </Link>
-      ))}
+      <Logo />
+      <div>
+        {navLinks.map(({ to, label }: NavLink) => (
+          <Link key={label} to={to}>
+            {label}
+          </Link>
+        ))}
+      </div>
     </header>
   );
 };
