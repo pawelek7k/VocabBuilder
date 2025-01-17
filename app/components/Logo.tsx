ccc84e1306afa394fe1df30955ea4b4847f4d62e
pawelek <k.pawelek@vp.pl>
2025-01-17 21:44:35 +0100

    Update Logo.tsx

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,3 +1,9 @@
 export const Logo = () => {
-  return <div></div>;
+  return (
+    <div className="bg-cyan-950 px-3 py-1 rounded-full">
+      <h1 className="">
+        Vocab<span className="font-semibold">Builder</span>
+      </h1>
+    </div>
+  );
 };
