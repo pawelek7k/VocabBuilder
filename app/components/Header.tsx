1cddca79193c99278ea33143fcea4edb55c273e1
pawelek <k.pawelek@vp.pl>
2025-01-17 20:17:12 +0100

    add nav links

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,14 @@
+import { NavLink, navLinks } from "../constants/navLinks";
+import { LinkComponent as Link } from "./Link";
+
 export const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      {navLinks.map(({ to, label }: NavLink) => (
+        <Link key={label} to={to}>
+          {label}
+        </Link>
+      ))}
+    </header>
+  );
 };
---
+++ app/constants/navLinks.ts
@@ -0,0 +1,10 @@
+export type NavLink = {
+    to: string;
+    label: string;
+}
+
+export const navLinks: NavLink[] = [
+    { to: "/dictionary", label: "Dictionary" },
+    { to: "/dictionary", label: "Recommend" },
+    { to: "/dictionary", label: "Training" },
+];
