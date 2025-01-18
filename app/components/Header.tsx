64b9bab72dac66b23d696dca41ec0289b09b7e7c
pawelek <k.pawelek@vp.pl>
2025-01-18 16:38:18 +0100

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="w-full flex justify-evenly border-b border-zinc-100">
+    <header className="w-full flex justify-evenly border-b border-zinc-100 items-center">
       <Logo />
       <div className="p-4 px-12 border-l-4 rounded-s-full">
         <ul className="flex gap-12">
