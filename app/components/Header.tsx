f8df0ef814adf4c1812d263629af96bfbdb0bbc1
pawelek <k.pawelek@vp.pl>
2025-01-18 14:44:09 +0100

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,21 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { NavLink, navLinks } from "../constants/arrays/navLinks";
 import { LinkComponent as Link } from "./Link";
 import { Logo } from "./Logo";
 
 export const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="p-4 w-full flex justify-between">
       <Logo />
       <ul className="flex gap-12">
         {navLinks.map(({ to, label }: NavLink) => (
           <li key={label}>
-            <Link to={to}>{label}</Link>
+            <Link to={to} className={pathname === to ? "active" : ""}>
+              {label}
+            </Link>
           </li>
         ))}
       </ul>
