fbabb3fdadd971df554c4a04f166dc06f954b8c2
pawelek <k.pawelek@vp.pl>
2025-01-18 17:17:01 +0100

    Update Logo.tsx

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <div className="bg-cyan-950 px-3 py-1 rounded-full flex items-center h-full">
+    <div className="bg-rose-100 text-zinc-950 px-3 py-1 rounded-full flex items-center h-full">
       <h1 className="">
         Vocab<span className="font-semibold">Builder</span>
       </h1>
