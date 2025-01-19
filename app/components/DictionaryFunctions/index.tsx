551c547bb4b6878e70bdceaf72707431f0d5fca6
pawelek <k.pawelek@vp.pl>
2025-01-19 15:15:57 +0100

    Update index.tsx

--- app/components/DictionaryFunctions/index.tsx
+++ app/components/DictionaryFunctions/index.tsx
@@ -3,11 +3,12 @@ import { SearchInput } from "./SearchInput";
 
 export const FuctionsContainer = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 flex justify-around">
       <div className="flex items-center gap-6">
         <SearchInput />
         <RadioChoice />
       </div>
+      <div></div>
     </div>
   );
 };
