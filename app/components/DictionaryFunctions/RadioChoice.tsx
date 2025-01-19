7231656a8f3d8f1460391037d1ff28334a448cba
pawelek <k.pawelek@vp.pl>
2025-01-19 16:14:54 +0100

    Update RadioChoice.tsx

--- app/components/DictionaryFunctions/RadioChoice.tsx
+++ app/components/DictionaryFunctions/RadioChoice.tsx
@@ -1,18 +1,20 @@
 import { options } from "@/app/constants/arrays/radioOptions";
+import { useId } from "react";
 
 export const RadioChoice = () => {
+  const uniqueId = useId();
   return (
     <ul className="flex gap-4">
       {options.map((option) => (
         <li key={option.id}>
           <label
-            htmlFor={option.id}
+            htmlFor={`${uniqueId}-${option.id}`}
             className="flex items-center gap-2 cursor-pointer"
           >
             <input
               type="radio"
-              name="choice"
-              id={option.id}
+              name={uniqueId}
+              id={`${uniqueId}-${option.id}`}
               className="appearance-none w-4 h-4 border border-gray-300 rounded-full checked:bg-pink-200 checked:border-pink-100"
             />
             <span className="text-zinc-200 font-semibold">{option.label}</span>
