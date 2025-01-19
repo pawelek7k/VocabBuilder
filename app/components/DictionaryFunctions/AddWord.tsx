aa63b60e2b2635f030b0e29d838c564d06fb3a77
pawelek <k.pawelek@vp.pl>
2025-01-19 15:18:39 +0100

    Create AddWord.tsx

---
+++ app/components/DictionaryFunctions/AddWord.tsx
@@ -0,0 +1,11 @@
+import { LuPlus } from "react-icons/lu";
+
+export const AddWord = () => {
+  return (
+    <div>
+      <button className="flex items-center gap-2">
+        Add word <LuPlus />
+      </button>
+    </div>
+  );
+};
