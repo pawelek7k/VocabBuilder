279c2329fde08496d7556574b3fdebe22b3cbbdf
pawelek <k.pawelek@vp.pl>
2025-01-19 15:17:18 +0100

    Update index.tsx

--- app/components/DictionaryFunctions/index.tsx
+++ app/components/DictionaryFunctions/index.tsx
@@ -1,14 +1,17 @@
 import { RadioChoice } from "./RadioChoice";
 import { SearchInput } from "./SearchInput";
+import { WordStudyLength } from "./WordStudyLength";
 
 export const FuctionsContainer = () => {
   return (
-    <div className="p-4 flex justify-around">
+    <div className="p-4 flex justify-around items-center">
       <div className="flex items-center gap-6">
         <SearchInput />
         <RadioChoice />
       </div>
-      <div></div>
+      <div>
+        <WordStudyLength />
+      </div>
     </div>
   );
 };
