a4141f6e7854a285e231333a7d996668f3026f48
pawelek <k.pawelek@vp.pl>
2025-01-19 15:20:53 +0100

    Create TrainLink.tsx

---
+++ app/components/DictionaryFunctions/TrainLink.tsx
@@ -0,0 +1,10 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+import { LinkComponent as Link } from "../Link";
+
+export const TrainLink = () => {
+  return (
+    <Link to="/training">
+      Train oneself <FaArrowRightLong />
+    </Link>
+  );
+};
