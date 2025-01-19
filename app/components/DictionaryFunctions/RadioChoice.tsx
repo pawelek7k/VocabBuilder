347f8569474b5a5eaa8e91e7385a140aed798f00
pawelek <k.pawelek@vp.pl>
2025-01-19 15:13:29 +0100

    add radio options

--- app/components/DictionaryFunctions/RadioChoice.tsx
+++ app/components/DictionaryFunctions/RadioChoice.tsx
@@ -1,8 +1,24 @@
+import { options } from "@/app/constants/arrays/radioOptions";
+
 export const RadioChoice = () => {
   return (
-    <div>
-      <input type="radio" name="" id="" />
-      <input type="radio" name="" id="" />
-    </div>
+    <ul className="flex gap-4">
+      {options.map((option) => (
+        <li key={option.id}>
+          <label
+            htmlFor={option.id}
+            className="flex items-center gap-2 cursor-pointer"
+          >
+            <input
+              type="radio"
+              name="choice"
+              id={option.id}
+              className="appearance-none w-4 h-4 border border-gray-300 rounded-full checked:bg-pink-200 checked:border-pink-100"
+            />
+            <span className="text-zinc-200 font-semibold">{option.label}</span>
+          </label>
+        </li>
+      ))}
+    </ul>
   );
 };
---
+++ app/constants/arrays/radioOptions.ts
@@ -0,0 +1,9 @@
+type RadioOption = {
+    id: string;
+    label: string;
+}
+
+export const options: RadioOption[] = [
+    { id: "regular", label: "Regular" },
+    { id: "irregular", label: "Irregular" },
+];
