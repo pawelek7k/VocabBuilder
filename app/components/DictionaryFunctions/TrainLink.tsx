e18fce7a28a6451f7f153aa85af94ea14dd43b2d
pawelek <k.pawelek@vp.pl>
2025-01-19 15:21:30 +0100

    Update TrainLink.tsx

--- app/components/DictionaryFunctions/TrainLink.tsx
+++ app/components/DictionaryFunctions/TrainLink.tsx
@@ -3,7 +3,7 @@ import { LinkComponent as Link } from "../Link";
 
 export const TrainLink = () => {
   return (
-    <Link to="/training">
+    <Link to="/training" className="flex items-center gap-2">
       Train oneself <FaArrowRightLong />
     </Link>
   );
