0254705a304034b3465baef5ea9ce4e64f0dae67
pawelek <k.pawelek@vp.pl>
2025-01-19 14:59:13 +0100

    install react icon and styled input

--- app/components/DictionaryFunctions/SearchInput.tsx
+++ app/components/DictionaryFunctions/SearchInput.tsx
@@ -1,3 +1,14 @@
+import { IoIosSearch } from "react-icons/io";
+
 export const SearchInput = () => {
-  return <input type="text" />;
+  return (
+    <div className="relative w-full max-w-52">
+      <input
+        type="text"
+        placeholder="Find the word"
+        className="w-full pr-10 pl-4 py-1 rounded-md border border-gray-300 focus:outline-none"
+      />
+      <IoIosSearch className="absolute text-zinc-950 top-1/2 right-3 transform -translate-y-1/2" />
+    </div>
+  );
 };
