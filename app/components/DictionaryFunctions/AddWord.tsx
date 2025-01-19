ad3dc7605b4c51d95f628e3081073d4d4fcdc92b
pawelek <k.pawelek@vp.pl>
2025-01-19 15:31:01 +0100

    Update AddWord.tsx

--- app/components/DictionaryFunctions/AddWord.tsx
+++ app/components/DictionaryFunctions/AddWord.tsx
@@ -1,11 +1,23 @@
+"use client";
+import { useState } from "react";
 import { LuPlus } from "react-icons/lu";
+import { AddWordModal } from "./AddWordModal";
 
 export const AddWord = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      <button className="flex items-center gap-2">
-        Add word <LuPlus />
-      </button>
-    </div>
+    <>
+      <div>
+        <button className="flex items-center gap-2" onClick={toggleModal}>
+          Add word <LuPlus />
+        </button>
+      </div>
+      {isModalOpen && <AddWordModal />}
+    </>
   );
 };
