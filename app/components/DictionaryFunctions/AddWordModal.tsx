324efb304f796af8f9a59ec027ffcd63f8a1cad1
pawelek <k.pawelek@vp.pl>
2025-01-22 18:04:13 +0100

    Update AddWordModal.tsx

--- app/components/DictionaryFunctions/AddWordModal.tsx
+++ app/components/DictionaryFunctions/AddWordModal.tsx
@@ -5,7 +5,7 @@ import { RadioChoice } from "./RadioChoice";
 export const AddWordModal = () => {
   return (
     <Overlay>
-      <div className="bg-white/70 backdrop-blur-md md:p-6 rounded-md justify-center w-full md:w-[50rem] dark:bg-black/70">
+      <div className="bg-white/70 backdrop-blur-md md:p-6 rounded-md flex gap-6 flex-col justify-center w-full md:w-[50rem] dark:bg-black/70">
         <h2>Add word</h2>
         <p>
           Adding a new word to the dictionary is an important step in enriching
